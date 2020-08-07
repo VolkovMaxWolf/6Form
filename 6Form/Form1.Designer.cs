a667d32ca8da9c6f369540fefe9746cf4b45859b
VolkovMaxWolf <68462998+VolkovMaxWolf@users.noreply.github.com>
2020-08-07 13:18:23 +0300

    Add files via upload

---
+++ 6Form/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace _6Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(18, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите размер";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(173, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Одномерный массив";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(371, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(290, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Двумерный массив";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(533, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(378, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Введите размер";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(305, 197);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(378, 137);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(305, 197);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(19, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 63);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Получить структуру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StructureForOneDimArrButton);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(378, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 63);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Получить структуру";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StructureForTwoDimArrButton);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(190, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 63);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Получить информацию";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.GetDataButton);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(549, 354);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 63);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Получить информацию";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.GetDataForTwoDimArrayButton);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(18, 460);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Сумма элементов";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(173, 457);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(151, 26);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(532, 457);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(151, 26);
+            this.textBox4.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(377, 460);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Сумма элементов";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(18, 493);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(146, 126);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Количество элементов, значения которых больше значения предыдущего элемента";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(170, 593);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(151, 26);
+            this.textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox6.Location = new System.Drawing.Point(532, 593);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(151, 26);
+            this.textBox6.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(377, 514);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 105);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Есть ли столбец, состоящий только из отрицательных элементов";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(380, 649);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(344, 20);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Произведение элементов каждого столбца";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(382, 688);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(342, 56);
+            this.dataGridView3.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox7.Location = new System.Drawing.Point(571, 761);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(112, 26);
+            this.textBox7.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(378, 764);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(187, 20);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Минимальный элемент";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox8.Location = new System.Drawing.Point(620, 84);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(52, 26);
+            this.textBox8.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 799);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ 6Form/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _6Form
+{
+    public partial class Form1 : Form
+    {
+        private int[] arr;
+        private int[,] twoArr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void StructureForOneDimArrButton(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && Сheck(textBox1.Text))
+            {
+                GetStricture(Convert.ToInt32(textBox1.Text));
+            }
+            
+        }
+        private void GetStricture(int size)
+        {
+            dataGridView1.ColumnCount = size;
+            arr = new int[size];
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+        private bool Сheck(String s)
+        {
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!char.IsDigit(arr[i]) && arr[i] != '-')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void GetDataButton(object sender, EventArgs e)
+        {
+            InitOneDimArr();
+            ShowOneDimArrInfo();
+        }
+
+        private void ShowOneDimArrInfo()
+        {
+            if (CheckSum(arr)) textBox3.Text = "двузначное число";
+            else textBox3.Text = "недвузначное число";
+            textBox5.Text = Convert.ToString(CountSum(arr));
+        }
+
+        private void InitOneDimArr()
+        {
+            for (int rows = 0; rows < dataGridView1.Rows.Count - 1; rows++)
+            {
+                for (int col = 0; col < dataGridView1.Rows[rows].Cells.Count; col++)
+                {
+                    arr[col] = Convert.ToInt32(dataGridView1.Rows[rows].Cells[col].Value);
+                }
+            }
+
+        }
+        private bool CheckSum(int[,] twoArr)
+        {
+            int sum = 0;
+            for (int i = 0; i < twoArr.GetLength(0); ++i)
+            {
+                for (int j = 0; j < twoArr.GetLength(1); ++j)
+                {
+                    sum += twoArr[i, j];
+                }
+            }
+            if (sum > -100 && sum < -9 || sum > 9 && sum < 100) return true;
+            return false;
+        }
+        private static bool CheckSum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; ++i) sum += arr[i];
+            if (sum > -100 && sum < -9 || sum > 9 && sum < 100) return true;
+            return false;
+        }
+        private int CountSum(int[] arr)
+        {
+            int neededCount = 0;
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] > arr[i - 1])
+                    neededCount++;
+            return neededCount;
+        }
+
+        private void StructureForTwoDimArrButton(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "" && Сheck(textBox2.Text) &&
+                textBox8.Text != "" && Сheck(textBox8.Text) && Convert.ToInt32(textBox2.Text) == Convert.ToInt32(textBox8.Text))
+            {
+                GetStricture(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox2.Text));
+            }
+        }
+        private void GetStricture(int lines, int columns)
+        {
+            dataGridView2.RowCount = lines;
+            dataGridView2.ColumnCount = columns;
+            twoArr = new int[lines, columns];
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        private void GetDataForTwoDimArrayButton(object sender, EventArgs e)
+        {
+            InitTwoDimArr();
+            ShowTwoDimArrInfo();
+        }
+
+        private void ShowTwoDimArrInfo()
+        {
+            if (CheckSum(twoArr)) textBox4.Text = "двузначное число";
+            else textBox4.Text = "не двузначное число";
+            if (CheckNegativeColumn(twoArr)) textBox6.Text = "есть";
+            else textBox6.Text = "отсутсвует";
+            int[][] alternativeTwoArray = GetAlternativeTwoDimArr();
+            int[] arr1 = GetMinMultFromRows(alternativeTwoArray);
+
+            dataGridView3.ColumnCount = arr1.Length;
+            dataGridView3.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            for (int rows = 0; rows < dataGridView3.Rows.Count; rows++)
+            {
+                for (int col = 0; col < dataGridView3.Rows[rows].Cells.Count; col++)
+                {
+                    dataGridView3.Rows[rows].Cells[col].Value = arr1[col];
+                }
+            }
+
+            textBox7.Text = arr1.Min().ToString();
+
+        }
+        private int[][] GetAlternativeTwoDimArr()
+        {
+            int[][] temp = new int[twoArr.GetLength(0)][];
+            for (int i = 0; i < twoArr.GetLength(0); i++)
+            {
+                temp[i] = new int[twoArr.GetLength(0)];
+                for (int j = 0; j < twoArr.GetLength(1); j++)
+                {
+                    temp[i][j] = twoArr[i, j];
+                }
+            }
+            return temp;
+
+        }
+
+        private void InitTwoDimArr()
+        {
+            for (int rows = 0; rows < dataGridView2.Rows.Count - 1; rows++)
+            {
+                for (int col = 0; col < dataGridView2.Rows[rows].Cells.Count; col++)
+                {
+                    twoArr[rows, col] = Convert.ToInt32(dataGridView2.Rows[rows].Cells[col].Value);
+                }
+            }
+        }
+        private bool CheckNegativeColumn(int[,] twoArr)
+        {
+            bool check = true;
+            for (int i = 0; i < twoArr.GetLength(0); ++i)
+            {
+                check = true;
+                for (int j = 0; j < twoArr.GetLength(1); ++j)
+                {
+                    if (twoArr[j, i] > 0) check = false;
+                }
+                if (check == true) return check;
+            }
+            return check;
+        }
+        private int[] GetMinMultFromRows(int[][] twoArr)
+        {
+            int[] a = new int[twoArr.GetLength(0)];
+            for (int i = 0; i < a.Length; ++i)
+            {
+                int temp = 1;
+
+                for (int j = 0; j <a.Length; ++j)
+                {
+                    temp *= twoArr[j][i];
+                }
+                a[i] = temp;
+            }
+            return a;
+        }
+    }
+
+}
